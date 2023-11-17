e40bc548b449079ed4ab55bdf824d665a332b204
Linda Paz <lindacsndelapaz@gmail.com>
2023-11-17 13:21:39 -0300

    fix screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,27 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container:{
+      padding: {
+        DEFAULD: '15px',
+      },
+    },
+    screens:{
+      sm: '640px',
+      md: '786px',
+      lg: '1024px',
+      xl: '1300px',
+    },
     extend: {
+      colors: {
+        primary: '#101828',
+        secondary: '#667085',
+        accent: {
+          DEFAULD: '#ed1d24',
+          hover: '#dd242a',
+        },
+        body:'#dedede',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
