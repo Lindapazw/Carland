da1057c7b8af86fe5321093255682197cbc0167f
Linda Paz <lindacsndelapaz@gmail.com>
2023-11-17 13:53:02 -0300

    add and imports components in page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,13 @@
-
+// components
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Cars from './components/Cars';
+import About from './components/About';
+import Why from './components/Why';
+import Testimonial from './components/Testimonial';
+import Cta from './components/Cta';
+import Footer from './components/Footer';
+import BackToTopBtn from './components/BackToTopBtn';
 
 export default function Home() {
   return (
