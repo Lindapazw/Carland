274cee941e23180a2d76b24556f9a507f9cf17f4
Linda Paz <lindacsndelapaz@gmail.com>
2023-11-18 12:57:06 -0300

    add SearchMobile styles

--- src/app/components/SearchMobile.js
+++ src/app/components/SearchMobile.js
@@ -1,8 +1,26 @@
-import React from 'react'
+// components
+import DateSelection from "./DateSelection";
+import HoursSelection from "./HoursSelection";
+import LocationSelection from "./LocationSelection";
 
 const SearchMobile = () => {
     return (
-        <div>SearchMobile</div>
+        <div className="xl:hidden font-medium">
+            <div className="container mx-auto">
+                <div className="flex flex-col gap-y-4">
+                    {/* location selection */}
+                    <LocationSelection/>
+                    {/* date selection */}
+                    <DateSelection/>
+                    {/* hours selection */}
+                    <HoursSelection/>
+                    {/* btn */}
+                    <div className="flex items-center px-6">
+                        <button className="btn btn-sm btn-accent w-[164px] mx-auto">Search</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
