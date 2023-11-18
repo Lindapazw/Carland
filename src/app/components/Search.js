60c007951574ed866b7756533f712c66875f2dff
Linda Paz <lindacsndelapaz@gmail.com>
2023-11-18 12:15:29 -0300

    add searchActive in Search

--- src/app/components/Search.js
+++ src/app/components/Search.js
@@ -1,8 +1,13 @@
-import React from 'react'
+"use client"
+
+// search context
+import { useContext } from "react"
+import { SearchContext } from "../context/search"
 
 const Search = () => {
+    const {searchActive} = useContext(SearchContext);      
     return (
-        <div>Search</div>
+        <div className={`${searchActive? 'bg-white rounded-none xl:h-[80px]' : 'bg-white rounded-[20px] py-6 xl:pr-4 xl:h-[98px]'} hidden xl:block w-full relative shadow-lg`}>Search</div>
     )
 }
 
