55bf5547b38a32767171691c66b71a7d2faeb9c7
Linda Paz <lindacsndelapaz@gmail.com>
2023-11-18 12:38:33 -0300

    add LocationSelection in Search component

--- src/app/components/Search.js
+++ src/app/components/Search.js
@@ -1,13 +1,24 @@
 "use client"
 
 // search context
-import { useContext } from "react"
-import { SearchContext } from "../context/search"
+import { useContext } from "react";
+import { SearchContext } from "../context/search";
+
+// Components
+import LocationSelection from "./LocationSelection";
 
 const Search = () => {
     const {searchActive} = useContext(SearchContext);      
     return (
-        <div className={`${searchActive? 'bg-white rounded-none xl:h-[80px]' : 'bg-white rounded-[20px] py-6 xl:pr-4 xl:h-[98px]'} hidden xl:block w-full relative shadow-lg`}>Search</div>
+        <div className={`${searchActive? 
+        'bg-white rounded-none xl:h-[80px]' : 
+        'bg-white rounded-[20px] py-6 xl:pr-4 xl:h-[98px]'
+        }hidden xl:block w-full relative shadow-lg`}
+        >
+            <div className="xl:h-full flex items-center px-6 xl:px-0">
+                <LocationSelection/>
+            </div>
+        </div>
     )
 }
 
