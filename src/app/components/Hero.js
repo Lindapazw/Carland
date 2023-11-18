8f8e26ae9854de93a8e7f2724a066eb26f5a65d4
Linda Paz <lindacsndelapaz@gmail.com>
2023-11-18 12:38:47 -0300

    add Search in Hero section

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -1,8 +1,31 @@
-import React from 'react'
+"use client"
+
+import { useContext } from "react"
+
+// Components
+import Search from "./Search"
+
+// context
+import { SearchContext } from "../context/search"
 
 const Hero = () => {
+
+    const {searchActive} = useContext(SearchContext)
+
     return (
-        <div>Hero</div>
+        <section className="h-screen xl:h-[90vh] bg-[#b2b7c2]/10" id="home">
+            <div className="container mx-auto h-full xl:pt-10">hero container</div>
+            {
+                searchActive ? (
+                <div className="fixed top-[80px] z-10 w-full max-w-[1920px]">
+                    <Search/>
+                </div>
+                ) : (
+                <div className="-mt-12 w-full max-w-[1300px] mx-auto">
+                    <Search/>
+                </div>
+                )}
+        </section>
     )
 }
 
