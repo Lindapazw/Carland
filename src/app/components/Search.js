fcf2b206164a24413e91676da130265679cafcfd
Linda Paz <lindacsndelapaz@gmail.com>
2023-11-19 12:45:01 -0300

    fix search component

--- src/app/components/Search.js
+++ src/app/components/Search.js
@@ -9,17 +9,22 @@ import LocationSelection from "./LocationSelection";
 
 const Search = () => {
     const {searchActive} = useContext(SearchContext);      
+
     return (
-        <div className={`${searchActive? 
-        'bg-white rounded-none xl:h-[80px]' : 
-        'bg-white rounded-[20px] py-6 xl:pr-4 xl:h-[98px]'
-        }hidden xl:block w-full relative shadow-lg`}
-        >
+        <div
+            className={`${
+                searchActive
+                ? 'bg-white rounded-none xl:h-[80px]'
+                : 'bg-white rounded-[20px] py-6 xl:pr-4 xl:h-[98px] hidden xl:block'
+                } w-full relative shadow-lg`}
+            >
+            {/* Contenido del componente */}
             <div className="xl:h-full flex items-center px-6 xl:px-0">
                 <LocationSelection/>
             </div>
         </div>
     )
+
 }
 
 export default Search
