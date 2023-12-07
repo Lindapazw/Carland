9ae946c0369fe7bdec14725167b6ed6d739f012f
Linda Paz <lindacsndelapaz@gmail.com>
2023-12-07 16:11:42 -0300

    add title in Hero component

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -14,7 +14,19 @@ const Hero = () => {
 
     return (
         <section className="h-screen xl:h-[90vh] bg-[#b2b7c2]/10" id="home">
-            <div className="container mx-auto h-full xl:pt-10">hero container</div>
+            <div className="container mx-auto h-full xl:pt-10">
+                {/* text & img wrapper */}
+                <div className="flex flex-col xl:flex-row justify-center items-center xl:justify-start h-full">
+                    {/* text */}
+                    <div className="text-center xl:max-w-xl xl:text-left mt-16 xl:mt-0">
+                        <h1 className="h1">
+                            Explore the Finest <span className="text-accent-DEFAULD">Global</span> Offers{' '}
+                        </h1>
+                    </div>
+                    {/* img */}
+                    <div>img</div>
+                </div>
+            </div>
             {
                 searchActive ? (
                 <div className="fixed top-[80px] z-10 w-full max-w-[1920px]">
