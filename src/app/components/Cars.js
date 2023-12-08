3076a6449865c87b8b866f6230895c8715e2c2ca
Linda Paz <lindacsndelapaz@gmail.com>
2023-12-08 14:36:06 -0300

    delete bg color in Cars section

--- src/app/components/Cars.js
+++ src/app/components/Cars.js
@@ -5,7 +5,7 @@ import CarSlider from "./CarSlider"
 
 const Cars = () => {
     return (
-        <section className='h-screen flex items-center bg-yellow-200' id='cars'>
+        <section className='h-screen flex items-center' id='cars'>
             <div className="container mx-auto">
                 <Brands/>
                 <CarSlider/>
