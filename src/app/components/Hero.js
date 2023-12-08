cded632ca4aa7bb39e7419ddbd442ac58a6adfba
Linda Paz <lindacsndelapaz@gmail.com>
2023-12-08 13:34:49 -0300

    add motion in Hero section

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -9,7 +9,13 @@ import Search from "./Search";
 import { SearchContext } from "../context/search";
 
 // next image
-import Image from 'next/image'
+import Image from 'next/image';
+
+// motion
+import {motion, easeInOut} from 'framer-motion';
+
+// variants
+import {fadeIn} from '/variants';
 
 const Hero = () => {
 
@@ -22,14 +28,32 @@ const Hero = () => {
                 <div className="flex flex-col xl:flex-row justify-center items-center xl:justify-start h-full">
                     {/* text */}
                     <div className="text-center xl:max-w-xl xl:text-left mt-16 xl:mt-0">
-                        <h1 className="h1">
+                        <motion.h1 
+                            variants={fadeIn('down', 0.2)} 
+                            initial='hidden' 
+                            whileInView={'show'} 
+                            viewport={{once: false, amount:0.6}} 
+                            className="h1"
+                        >
                             Explore the Finest <span className="text-accent-DEFAULD">Global</span> Offers{' '}
-                        </h1>
-                        <p className="description max-w-[550px] mx-auto xl:mx-0 mb-6 xl:mb-10"> 
+                        </motion.h1>
+                        <motion.p 
+                            variants={fadeIn('down', 0.4)} 
+                            initial='hidden' 
+                            whileInView={'show'} 
+                            viewport={{once: false, amount:0.6}} 
+                            className="description max-w-[550px] mx-auto xl:mx-0 mb-6 xl:mb-10"
+                        > 
                             Find your ideal ride for any adventure with our diverse range of affordable and dependable car rentals.
-                        </p>
+                        </motion.p>
                         {/* btns */}
-                        <div className="flex gap-x-3 justify-center xl:justify-start">
+                        <motion.div 
+                            variants={fadeIn('down', 0.6)} 
+                            initial='hidden' 
+                            whileInView={'show'} 
+                            viewport={{once: false, amount:0.8}} 
+                            className="flex gap-x-3 justify-center xl:justify-start"
+                        >
                             {/* btn appstore */}
                             <button className="btn-cta">
                                 <Image src={'/icons/buttons/app-store.svg'} width={132} height={36} alt="app store"/>
@@ -38,22 +62,41 @@ const Hero = () => {
                             <button className="btn-cta">
                                 <Image src={'/icons/buttons/google-play.svg'} width={132} height={36} alt="google play"/>
                             </button>
-                        </div>
+                        </motion.div>
                     </div>
                     {/* img car*/}
-                    <div className="relative w-full h-full max-w-[50vh] md:max-w-[70vw] xl:max-w-[860px] xl:max-h-[542px] xl:absolute xl:-right-[100px] min-[1680px]:right-[120px] xl:top-48">
+                    <motion.div 
+                        variants={fadeIn('up', 0.6)} 
+                        initial='hidden' 
+                        whileInView={'show'} 
+                        viewport={{once: false, amount:0.6}} 
+                        className="relative w-full h-full max-w-[50vh] md:max-w-[70vw] xl:max-w-[860px] xl:max-h-[542px] xl:absolute xl:-right-[100px] min-[1680px]:right-[120px] xl:top-48"
+                    >
                         <Image src={'/images/hero/car.svg'} alt="car" fill style={{objectFit:'contain'}} priority/>
-                    </div>
+                    </motion.div>
                 </div>
             </div>
             {
                 searchActive ? (
-                <div className="fixed top-[80px] z-10 w-full max-w-[1920px]">
+                <motion.div 
+                    initial={{y: '-100%'}}
+                    animate={{y: 0}}
+                    transition={{ease: easeInOut}}
+                    className="fixed top-[80px] z-10 w-full max-w-[1920px]"
+                >
                     <Search/>
-                </div>
+                </motion.div>
                 ) : (
-                <div className="-mt-12 w-full max-w-[1300px] mx-auto">
-                    <Search/>
+                <div className="-mt-">
+                    <motion.div
+                        variants={fadeIn('up', 0.8)} 
+                        initial='hidden' 
+                        whileInView={'show'} 
+                        viewport={{once: false, amount:0.2}} 
+                        className="-mt-12 w-full max-w-[1300px] mx-auto"
+                    >
+                        <Search/>
+                    </motion.div>
                 </div>
                 )}
         </section>
