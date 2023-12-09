6e62579197d249e5556be4f404ef0296232d7f67
Linda Paz <lindacsndelapaz@gmail.com>
2023-12-09 12:18:10 -0300

    add stars

--- src/app/components/CarSlider.js
+++ src/app/components/CarSlider.js
@@ -148,26 +148,34 @@ const CarSlider = () => {
             }}>
                 {cars.map((car, index)=>{
                     return <SwiperSlide key={index}>
-                        <div className='max-w-[385px] mx-auto sm:mx-0 bg-gray-200'>
+                        <div className='max-w-[385px] mx-auto sm:mx-0'>
                             <Image src={car.image} width={380} height={284} alt='slider'/>
-                            <div>
+                            <div className='flex justify-between'>
                                 <div>
-                                    <div>{car.typeof}</div>
-                                    <h3>{car.name}</h3>
-                                    <h3>{car.price}/day</h3>
+                                    <div className='text-[13px] text-secondary uppercase'>{car.typeof}</div>
+                                    <h3 className='text-lg uppercase font-bold'>{car.name}</h3>
+                                    <div className='mb-10 text-accent-DEFAULD font-semibold uppercase'>
+                                        {car.price}/day
+                                    </div>
                                 </div>
                                 {/* stars */}
-                                <div>stars</div>
+                                <div className='flex gap-x-2 text-accent-DEFAULD h-max'>
+                                    <FaStar/>
+                                    <FaStar/>
+                                    <FaStar/>
+                                    <FaStar/>
+                                    <FaStar/>
+                                </div>
                             </div>
                             {/* car info */}
                             <div className='flex gap-x-3 xl:gap-x-4 w-max mb-10'>
                                 {car.info.map((item, index)=> {
                                     return <div key={index} className='flex flex-col items-center'>
-                                        <div className='bg-primary w-12 h-12 rounded-full flex justify-center items-center'>
+                                        <div className='bg-primary w-12 h-12 rounded-full flex justify-center items-center mb-2'>
                                             <Image 
                                                 src={item.icon} 
-                                                width={24} 
-                                                height={24} 
+                                                width={20} 
+                                                height={20} 
                                                 alt='stars'
                                             />
                                         </div>
