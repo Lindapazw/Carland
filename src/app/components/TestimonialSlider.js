1d4e20f1a6567447c33a81e2cb568645ed635b38
Linda Paz <lindacsndelapaz@gmail.com>
2023-12-10 14:38:33 -0300

    add import in testomonial section

--- src/app/components/TestimonialSlider.js
+++ src/app/components/TestimonialSlider.js
@@ -1,4 +1,28 @@
-import React from 'react'
+"use client"
+
+// import swipper react components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// import swipper styles
+import 'swipper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import {Pagination} from 'swipper/modules';
+
+// icons
+import { FaQuoteLeft } from 'react-icons/fa';
+
+// framer motion
+import {motion} from 'framer-motion';
+
+// variants
+import {fadeIn} from '/variants';
+
+// next image
+import Image from 'next/image';
+
+
 
 const TestimonialSlider = () => {
     return (
