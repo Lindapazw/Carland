6b589650be56e3e36d45ec4f0a1d346cc3056694
Linda Paz <lindacsndelapaz@gmail.com>
2023-12-10 14:10:04 -0300

    add structure in why us section

--- src/app/components/Why.js
+++ src/app/components/Why.js
@@ -1,8 +1,29 @@
+"use client"
 
+// next image
+import Image from 'next/image';
+
+// framer motion
+import {motion} from 'framer-motion';
+
+// variants
+import { fadeIn } from '/variants';
+
+// icons
+import { MdHandshake, MdKey, MdTrendingUp } from 'react-icons/md'
 
 const Why = () => {
     return (
-        <section className="section flex items-center bg-green-300" id="why">Why</section>
+        <section className="section flex items-center" id="why">
+            <div className='container mx-auto'>
+                <h2> Unmatched excellence and customer satisfaction </h2>
+                <p> Our dedication to providing exceptional services sets us apart from the competition. From the moment you engage with us, we strive to exceed your ecpectations in every interaction.</p>
+                {/* car image */}
+                <div>
+                    <Image src={'/images/why/car.svg'} width={1060} height={420} alt='car why us'/>
+                </div>
+            </div>
+        </section>
     )
 }
 
