6d581e4a797ca4a32a20ac1671381a2748cc420a
Linda Paz <lindacsndelapaz@gmail.com>
2023-12-10 14:58:26 -0300

    delete bg

--- src/app/components/Testimonial.js
+++ src/app/components/Testimonial.js
@@ -1,11 +1,9 @@
-"use client"
-
 // components
 import TestimonialSlider from "./TestimonialSlider";
 
 const Testimonial = () => {
     return (
-        <section className='section flex items-center bg-teal-300' id='testimonials'>
+        <section className='section flex items-center' id='testimonials'>
             <div className="container mx-auto">
                 <TestimonialSlider/>
             </div>
