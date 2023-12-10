fe49a11c8f21b2548bc310a2947a376006b1dfe7
Linda Paz <lindacsndelapaz@gmail.com>
2023-12-10 16:28:17 -0300

    add structure in footer section

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,8 +1,41 @@
-import React from 'react'
+"use client"
+
+// next image
+import Image from 'next/image';
+
+// icons
+import {FaPhone, FaEnvelope} from 'react-icons/fa6';
+
+// components
+import Copyright from './Copyright';
+
+// motions
+import {motion} from 'framer-motion';
+
+// variants
+import {fadeIn} from '/variants';
+
+// link / react scroll
+import {Link} from 'react-scroll';
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer className='pt-20 bg-white z-20'>
+            <div className='container mx-auto mb-24'>
+                {/* grid */}
+                <div className='flex flex-col lg:flex-row lg:justify-between gap-x-5 gap-y-14'>
+                    <div>
+                        {/* logo */}
+                        <Link to={'home'} smooth={true} spy={true} className='cursor-pointer'>
+                            <Image src={'/icons/logo.svg'} width={200} height={200} alt='footer grid'/>
+                        </Link>
+                    </div>
+                    <div>1</div>
+                    <div>1</div>
+                    <div>1</div>
+                </div>
+            </div>
+        </footer>
     )
 }
 
