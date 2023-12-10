25e717bf6b8c143ca94d963e41f1027045e51ba7
Linda Paz <lindacsndelapaz@gmail.com>
2023-12-10 15:12:50 -0300

    add TestimonialSlider structure

--- src/app/components/TestimonialSlider.js
+++ src/app/components/TestimonialSlider.js
@@ -40,7 +40,32 @@ const testimonialsData = [
 
 const TestimonialSlider = () => {
     return (
-        <div>TestimonialSlider</div>
+        <div>
+            <Swiper pagination={{
+                clickable:true,
+                dynamicBullets:true,
+            }}
+                modules={[Pagination]}
+                className='h-[450px]'
+            >
+                {testimonialsData.map((person, index)=> {
+                    const {message, avatar, name, job} = person;
+                    return (
+                    <SwiperSlide key={index}>
+                        <div className='flex flex-col justify-center items-center text-center'>
+                            <FaQuoteLeft className='text-7xl text-accent-DEFAULD mb-6'/>
+                            <div className='text-2xl xl:text-4xl max-w-[874px] mb-12 font-medium'>
+                                {message}
+                            </div>
+                            <Image src={avatar} width={64} height={64} alt='testimonial' className='mb-4'/>
+                            <div className='text-lg font-medium'>{name}</div>
+                            <div className='text-secondary'>{job}</div>
+                        </div>
+                    </SwiperSlide>
+                    );
+                })}
+            </Swiper>
+        </div>
     )
 }
 
