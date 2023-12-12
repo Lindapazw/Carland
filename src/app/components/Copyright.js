a80b073ef8e6b5656884464b1dc6d8aa61417c91
Linda Paz <lindacsndelapaz@gmail.com>
2023-12-12 10:41:03 -0300

    fix name un Copyright section

--- src/app/components/Copyright.js
+++ src/app/components/Copyright.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Copyright = () => {
     return (
         <div className='text-center py-10 border-t text-sm'>
-            Copyright &copy; LindaPazW 2023. All rights reserved.
+            Copyright &copy; Lindapazw 2023. All rights reserved.
         </div>
     )
 }
