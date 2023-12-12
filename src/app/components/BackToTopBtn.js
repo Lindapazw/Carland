ffb01b8ece20523044c493f9cdc19a8a08d2cca7
Linda Paz <lindacsndelapaz@gmail.com>
2023-12-12 10:40:40 -0300

    add button in BackToTopBtn

--- src/app/components/BackToTopBtn.js
+++ src/app/components/BackToTopBtn.js
@@ -1,8 +1,19 @@
-import React from 'react'
+"use client"
+
+import React, {useEffect, useState} from "react";
+
+// icons
+import {FaChevronUp} from 'react-icons/fa';
+
+// react scroll
+import { Link } from 'react-scroll';
 
 const BackToTopBtn = () => {
+    const [isActive, setActive] = useState(false)
     return (
-        <div>BackToTopBtn</div>
+        <Link className="fixed bg-accent-DEFAULD hover:bg-accent-hover w-12 h-12 right-16 bottom-11 z-10 cursor-pointer flex justify-center items-center text-white border-2 border-white">
+        <FaChevronUp className="text-xl"/>
+        </Link>
     )
 }
 
