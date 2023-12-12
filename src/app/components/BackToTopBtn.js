6088aa58799c091d85a689d00846383b94a31cd1
Linda Paz <lindacsndelapaz@gmail.com>
2023-12-12 10:58:58 -0300

    add BackToTopBtn

--- src/app/components/BackToTopBtn.js
+++ src/app/components/BackToTopBtn.js
@@ -9,9 +9,28 @@ import {FaChevronUp} from 'react-icons/fa';
 import { Link } from 'react-scroll';
 
 const BackToTopBtn = () => {
-    const [isActive, setActive] = useState(false)
+    const [isActive, setIsActive] = useState(true);
+    useEffect(() => {
+        const handleScroll = () => {
+            if(window.scrollY > 400){
+                setIsActive(true)
+            } else {
+                setIsActive(false)
+            }
+        };
+        window.addEventListener('scroll', handleScroll);
+
+        // clean up the listeners
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    });
     return (
-        <Link className="fixed bg-accent-DEFAULD hover:bg-accent-hover w-12 h-12 right-16 bottom-11 z-10 cursor-pointer flex justify-center items-center text-white border-2 border-white">
+        <Link 
+        to="home" 
+        smooth={true} 
+        className={`${!isActive && 'hidden'} fixed bg-accent-DEFAULD hover:bg-accent-hover w-12 h-12 right-16 bottom-11 z-10 cursor-pointer flex justify-center items-center text-white border-2 border-white`}
+        >
         <FaChevronUp className="text-xl"/>
         </Link>
     )
