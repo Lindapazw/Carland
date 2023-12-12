7d208400dfd7c4dc937cbef51ad2d33e1c444e73
Linda Paz <lindacsndelapaz@gmail.com>
2023-12-12 10:31:50 -0300

    add Copyright structure

--- src/app/components/Copyright.js
+++ src/app/components/Copyright.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Copyright = () => {
     return (
-        <div>Copyright</div>
+        <div className='text-center py-10 border-t text-sm'>
+            Copyright &copy; LindaPazW 2023. All rights reserved.
+        </div>
     )
 }
 
